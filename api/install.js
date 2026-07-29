0d4d136c90d8a7b2dccc0d5f4b512caf615184d0
Mell <141835304+mellkarine@users.noreply.github.com>
2026-07-29 17:11:48 -0300

    Update install.js

--- api/install.js
+++ api/install.js
@@ -1,12 +1,24 @@
 export default function handler(req, res) {
   res.status(200).send(`
-    <!DOCTYPE html>
-    <html>
-      <body>
-        <script>
-          window.location.href = "/";
-        </script>
-      </body>
-    </html>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <script src="https://api.bitrix24.com/api/v1/"></script>
+</head>
+<body>
+  <p>Finalizando instalação...</p>
+
+  <script>
+    BX24.init(function () {
+      BX24.installFinish();
+
+      setTimeout(function () {
+        window.location.href = "/";
+      }, 500);
+    });
+  </script>
+</body>
+</html>
   `);
 }
